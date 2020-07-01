c3c35437d1908bdcd072b466673a3e0906675a3d
Rodrigo Arcanjo <arcanjomitsuo@gmail.com>
2020-07-01 09:58:20 -0300

    Entrega desafio fundamentos react native

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -1,19 +1,15 @@
-import React, { useState, useMemo } from 'react';
-
 import { useNavigation } from '@react-navigation/native';
-
+import React, { useMemo } from 'react';
 import FeatherIcon from 'react-native-vector-icons/Feather';
+import { useCart } from '../../hooks/cart';
+import formatValue from '../../utils/formatValue';
 import {
-  Container,
-  CartPricing,
   CartButton,
   CartButtonText,
+  CartPricing,
   CartTotalPrice,
+  Container,
 } from './styles';
-
-import formatValue from '../../utils/formatValue';
-
-import { useCart } from '../../hooks/cart';
 
 // Calculo do total
 // Navegação no clique do TouchableHighlight
@@ -24,13 +20,23 @@ const FloatingCart: React.FC = () => {
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    const total = products.reduce((accumulator, product) => {
+      const productsSubtotal = product.price * product.quantity;
 
-    return formatValue(0);
+      return accumulator + productsSubtotal;
+    }, 0);
+
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((accumulator, product) => {
+      const productsQuantity = product.quantity;
+
+      return accumulator + productsQuantity;
+    }, 0);
+
+    return total;
 
     return 0;
   }, [products]);
